ade90b6c846a78ae8609ebbc2c7f83b21ff20406
muffler01 <muffler01@naver.com>
2021-01-07 04:26:03 +0900

    fix

--- baekjoon/pratice/15596 정수 N개의 합.py
+++ baekjoon/pratice/15596 정수 N개의 합.py
@@ -1,11 +1,5 @@
-# a : 합을 구해야 하는 정수 n 개가 저장되어 있는 리스트
+﻿# a : 합을 구해야 하는 정수 n 개가 저장되어 있는 리스트
 # return : a안의 정수 합
 
-def IntSum(sum_list):
-    sum_value = 0
-    for i in sum_list:
-        sum_value += i
-    return sum_value
-
-sum_list = [1,2,3,4]
-print(IntSum(sum_list))
+def solve(a):
+    return sum(a)
